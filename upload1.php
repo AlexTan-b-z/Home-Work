58648af3787432a2ec7d7611fdb54cd8cc332f2c
AlexTan-b-z <572455105@qq.com>
2015-12-11 20:27:19 +0800

    Update upload1.php

--- upload1.php
+++ upload1.php
@@ -9,7 +9,7 @@
 	<label for="file">文件名称:</label>
 	<input type="file" name="file" id="file" />
 	<br />
-	<input type="text" name="yzm" id="yzm" value="请输入验证码,点击图片刷新">
+	<input type="text" name="yzm" id="yzm" placeholder="请输入验证码,点击图片刷新">
 	<img src="yzm.php" title="点击刷新" align="absbottom" onclick="this.src='yzm.php?'+Math.random();">
 	<br /> 
 	<input type="submit"name="submit"value="上传">
